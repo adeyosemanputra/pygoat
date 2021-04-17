5d2f0b517a1fdd5de47bcfafcd4baec08e857d11
pwned-17 <shaikajmal.r2000@hotmail.com>
2021-04-18 00:49:25 +0530

    XXE Working Lab Developed

--- pygoat/introduction/static/Lab/xss.js
+++ pygoat/introduction/static/Lab/xss.js
@@ -12,5 +12,17 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-p=document.getElementById("t")
-p.innerHTML="alert(1)"
+function SendToServer(){
+
+        comment=document.getElementById("comment").value;
+
+
+        var xhr;
+        xhr = new XMLHttpRequest();
+        xml="<?xml version='1.0'?>"+"<comm>"+"<text>"+comment+"</text>"+"</comm>";
+       var url="http://127.0.0.1:2000/xxe_parse";
+       xhr.open("POST", url, true);
+       xhr.setRequestHeader("Content-Type", "text/xml");
+       xhr.send(xml);
+
+}
