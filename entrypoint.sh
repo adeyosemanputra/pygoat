7dcf207ae9cb0fbcdbd0a0195905bc782dc542e5
adeyosemanputra <ade.putra@owasp.org>
2021-05-10 21:43:10 +0700

    fix

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Collect static files
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
