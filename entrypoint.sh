9c17553626127f0863f3ec24c7fb4f5cf8d1e61b
adeyosemanputra <ade.putra@owasp.org>
2021-05-10 22:33:29 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python3 manage.py migrate
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
