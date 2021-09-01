b2246f6153cc56b63fa7de439ea9c3f70aba86f1
msant262 <msant262@gmail.com>
2021-09-01 10:37:59 -0300

    teste2

--- pygoat/manage.py
+++ pygoat/manage.py
@@ -13,6 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "teste"
         ) from exc
     execute_from_command_line(sys.argv)
 
