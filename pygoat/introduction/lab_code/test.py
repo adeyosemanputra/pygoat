8fa946bda778faca29f58da5a9b146633142b785
DigvijayBhosale1729 <digvijayb1729@gmail.com>
2022-01-18 17:29:20 +0530

    Changed Description and slightly altered working of A9

--- pygoat/introduction/lab_code/test.py
+++ pygoat/introduction/lab_code/test.py
@@ -1,3 +1,4 @@
+'''
 import subprocess, json
 
 
@@ -13,3 +14,16 @@ stderr = stderr.decode('utf-8')
 # res = json.loads(data)
 # print("Stdout\n" + data)
 print(data + stderr)
+'''
+import yaml, subprocess
+stream = open('/home/fox/test.yaml', 'r')
+data = yaml.load(stream)
+
+'''
+stdout, stderr = data.communicate()
+stdout = stdout.decode('utf-8')
+stderr = stderr.decode('utf-8')
+'''
+print(data + "\n")
+# print(stdout + "\n")
+# print(stderr + "\n")
