4eae47b569c32caed6102110a47c2566548e4783
gaurav-katiyar <85770903+gaurav-katiyar@users.noreply.github.com>
2022-03-14 10:46:21 +0000

    added new vulns

--- pygoat/introduction/views.py
+++ pygoat/introduction/views.py
@@ -356,6 +356,44 @@ def cmd_lab(request):
             return render(request, 'Lab/CMD/cmd_lab.html')
     else:
         return redirect('login')
+    
+    
+@csrf_exempt
+def cmd_lab2(request):
+    if request.user.is_authenticated:
+        if(request.method=="POST"):
+            domain=request.POST.get('domain')
+            domain=domain.replace("https://www.",'')
+            os=request.POST.get('os')
+            print(os)
+            if(os=='win'):
+                command="nslookup {}".format(domain)
+            else:
+                command = "dig {}".format(domain)
+            
+            try:
+                # output=subprocess.check_output(command,shell=True,encoding="UTF-8")
+                process = subprocess.Popen(
+                    command,
+                    shell=True,
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.PIPE)
+                stdout, stderr = process.communicate()
+                data = stdout.decode('utf-8')
+                stderr = stderr.decode('utf-8')
+                # res = json.loads(data)
+                # print("Stdout\n" + data)
+                output = data + stderr
+                print(data + stderr)
+            except:
+                output = "Something went wrong"
+                return render(request,'Lab/CMD/cmd_lab.html',{"output":output})
+            print(output)
+            return render(request,'Lab/CMD/cmd_lab.html',{"output":output})
+        else:
+            return render(request, 'Lab/CMD/cmd_lab.html')
+    else:
+        return redirect('login')
 
 #******************************************Broken Authentication**************************************************#
 
@@ -455,6 +493,31 @@ def a9_lab(request):
                 return render(request, "Lab/A9/a9_lab.html", {"data":"Please Upload a Yaml file."})
     else:
         return redirect('login')
+    
+#*********************************************************A10*************************************************#
+
+def a9_lab2(request):
+    if request.user.is_authenticated:
+        if request.method=="GET":
+            return render(request,"Lab/A9/a9_lab.html")
+        else:
+
+            try :
+                file=request.FILES["file"]
+                try :
+                    data = yaml.load(file)
+                    
+                    return render(request,"Lab/A9/a9_lab.html",{"data":data})
+                except:
+                    return render(request, "Lab/A9/a9_lab.html", {"data": "Error"})
+
+            except:
+                return render(request, "Lab/A9/a9_lab.html", {"data":"Please Upload a Yaml file."})
+    else:
+        return redirect('login')
+    
+    
+    
 def get_version(request):
       return render(request,"Lab/A9/a9_lab.html",{"version":"pyyaml v5.1"})
 
