f5989e801b8944f57e7f9908f38cceefec7bc452
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-03-22 02:48:51 +0530

    resolved issue #97

--- pygoat/introduction/models.py
+++ pygoat/introduction/models.py
@@ -4,21 +4,25 @@ from django.conf import settings
 # Create your models here.
 
 class FAANG (models.Model):
+    id = models.AutoField(primary_key=True)
     company=models.CharField(max_length=200);
     def __str__(self):
         return self.company;
 
 class info(models.Model):
+    id = models.AutoField(primary_key=True)
     faang=models.ForeignKey(to=FAANG,on_delete=models.CASCADE)
 
     ceo=models.CharField(max_length=200)
     about=models.CharField(max_length=200)
 
 class login(models.Model):
+    id = models.AutoField(primary_key=True)
     user=models.CharField(max_length=200)
     password=models.CharField(max_length=300)
 
 class comments(models.Model):
+    id = models.AutoField(primary_key=True)
     name=models.CharField(max_length=200)
     comment=models.CharField(max_length=600)
 
@@ -29,12 +33,14 @@ class authLogin(models.Model):
     userid = models.AutoField(primary_key=True)
 
 class otp(models.Model):
+    id = models.AutoField(primary_key=True)
     email=models.CharField(max_length=200)
     otp=models.IntegerField(validators=[MaxValueValidator(300)])
 
 class tickits(models.Model):
+    id = models.AutoField(primary_key=True)
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     tickit=models.CharField(max_length=40, unique = True)
 
     def __str__(self):
-        return self.tickit+ " " + self.user.username;
+        return self.tickit+ " " + self.user.username; 
