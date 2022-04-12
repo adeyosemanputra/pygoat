a7d57c4fd139ab074fabae31da3f49bf39a07961
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-04-12 12:03:50 +0530

    added a view code option in the labitself to see the source code

--- pygoat/introduction/static/Lab/xss.js
+++ pygoat/introduction/static/Lab/xss.js
@@ -1,8 +1,20 @@
 var coll = document.getElementsByClassName("coll");
+var coll2 = document.getElementsByClassName("coll2");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+for (i = 0; i < coll2.length; i++) {
+  coll2[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
