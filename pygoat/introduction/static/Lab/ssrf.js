1854a88573840bbde426992e25539b5ee4c63066
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-04-29 10:47:28 +0530

    ssrf lab1 playground stage1 50% checkpoint

---
+++ pygoat/introduction/static/Lab/ssrf.js
@@ -0,0 +1,9 @@
+// var frame1 = document.getElementsByClassName('ssrf-frame-1')
+
+function frame1to2(){
+    // frame 1 to 2
+    document.getElementById('ssrf-frame-1').style.display = 'none';
+    document.getElementById('ssrf-frame-2').style.display = 'flex';
+    document.getElementById('ssrf-progress-bar').style.display = 'flex';
+}
+
