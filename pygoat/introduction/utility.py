4294e16c6af1856e4cd948b255d8749af02f4450
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-05-08 19:50:19 +0530

    html input extractor added

--- pygoat/introduction/utility.py
+++ pygoat/introduction/utility.py
@@ -1,33 +1,5 @@
 import os
-
-input_code = '''def ssrf_lab(request):
-    if request.user.is_authenticated:
-        if request.method=="GET":
-            return render(request,"Lab/ssrf/ssrf_lab.html",{"blog":"Read Blog About SSRF"})
-        else:
-            file=request.POST["blog"]
-            try :
-                dirname = os.path.dirname(__file__)
-                filename = os.path.join(dirname, file)
-                file = open(filename,"r")
-                data = file.read()
-                return render(request,"Lab/ssrf/ssrf_lab.html",{"blog":data})
-            except:
-                return render(request, "Lab/ssrf/ssrf_lab.html", {"blog": "No blog found"})
-    else:
-        return redirect('login')'''
-
-output_code = '''import os
-def ssrf_lab(file):
-    try :
-        dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, file)
-        file = open(filename,"r")
-        data = file.read()
-        return {"blog":data}
-    except:
-        return {"blog": "No blog found"}'''
-
+# import re
 def ssrf_code_converter(code):
     list_input = code.split("\n")
     list_output = ['import os','def ssrf_lab(file):','    try:']
@@ -49,4 +21,14 @@ def ssrf_code_converter(code):
     f.close()
     return output_Code
 
-ssrf_code_converter(input_code)
+# ssrf_code_converter(input_code)
+def ssrf_html_input_extractor(code):
+    params = []
+    list_input = code.split("\n")
+    tokens = list(map(lambda x : x.strip().split(' '), list_input))
+    for i in range(len(tokens)):
+        if tokens[i][0] == '<input':
+            for j in range(len(tokens[i])):
+                if tokens[i][j][:7] == 'value="':
+                    params.append(tokens[i][j][7:-2])
+    return params
