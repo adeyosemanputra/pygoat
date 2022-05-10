ca1dd37282b8892a39f30ff178dfb7d7475273cb
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-05-10 12:18:59 +0530

    improved utilty function for code convertion

--- pygoat/introduction/utility.py
+++ pygoat/introduction/utility.py
@@ -5,13 +5,18 @@ def ssrf_code_converter(code):
     list_output = ['import os','def ssrf_lab(file):','    try:']
     extracted_code = []
     i = 7
-    while i < (len(list_input)-5):
+    while i < (len(list_input)-2):
         extracted_code.append(list_input[i][8:])
         i += 1
+
+    for i in range(len(extracted_code)):
+        if extracted_code[i].strip()[:6] == 'return':
+            space = extracted_code[i].split('return')[0]
+            print(space)
+            k = extracted_code[i].split('{')[1].split('}')[0]
+            extracted_code[i] = space + "return {"+k+"}"
+    
     list_output= list_output + extracted_code
-    list_output.append('        return {"blog":data}')
-    list_output.append("    except:")
-    list_output.append('        return {"blog": "No blog found"}')
     output_Code = "\n".join(list_output)
 
     dirname = os.path.dirname(__file__)
