f9eaa9ada5de93a2eb79bae1f1db0358f87cf1d6
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-08-03 13:23:13 +0530

    mitre views added for mitre 1-25

--- pygoat/introduction/mitre.py
+++ pygoat/introduction/mitre.py
@@ -14,3 +14,123 @@ def mitre_top1(request):
 def mitre_top2(request):
     if request.method == 'GET':
         return render(request, 'mitre/mitre_top2.html')
+
+@authentication_decorator
+def mitre_top3(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top3.html')
+        
+@authentication_decorator
+def mitre_top4(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top4.html')
+        
+@authentication_decorator
+def mitre_top5(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top5.html')
+        
+@authentication_decorator
+def mitre_top6(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top6.html')
+        
+@authentication_decorator
+def mitre_top7(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top7.html')
+        
+@authentication_decorator
+def mitre_top8(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top8.html')
+        
+@authentication_decorator
+def mitre_top9(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top9.html')
+        
+@authentication_decorator
+def mitre_top10(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top10.html')
+        
+@authentication_decorator
+def mitre_top11(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top11.html')
+        
+@authentication_decorator
+def mitre_top12(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top12.html')
+        
+@authentication_decorator
+def mitre_top13(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top13.html')
+        
+@authentication_decorator
+def mitre_top14(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top14.html')
+
+@authentication_decorator
+def mitre_top15(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top15.html')
+
+@authentication_decorator
+def mitre_top16(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top16.html')
+
+@authentication_decorator
+def mitre_top17(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top17.html')
+
+@authentication_decorator
+def mitre_top18(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top18.html')
+
+@authentication_decorator
+def mitre_top19(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top19.html')
+
+
+@authentication_decorator
+def mitre_top20(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top20.html')
+
+
+@authentication_decorator
+def mitre_top21(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top21.html')
+
+
+@authentication_decorator
+def mitre_top22(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top22.html')
+
+
+@authentication_decorator
+def mitre_top23(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top23.html')
+
+
+@authentication_decorator
+def mitre_top24(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top24.html')
+
+@authentication_decorator
+def mitre_top25(request):
+    if request.method == 'GET':
+        return render(request, 'mitre/mitre_top25.html')
