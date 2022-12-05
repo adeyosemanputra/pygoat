2742c4a1515bfee055265cd1b5573db7f1fb0d75
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2022-12-05 17:59:39 -0500

    chrome support

--- pygoat/introduction/views.py
+++ pygoat/introduction/views.py
@@ -94,6 +94,11 @@ def xss_p_lab(request):
     else:
         return redirect('login')
 
+def xss_d_lab(request):
+    if request.user.is_authenticated:
+        return render(request,"Lab/XSS/xss_d_lab.html")
+    else:
+        return redirect('login')
 
 def xss_r_lab(request):
     companies=FAANG.objects.all()
@@ -117,9 +122,9 @@ def xss_r_lab(request):
         f=FAANG.objects.all().filter(company=q)
         if f:
             args={"company":f[0].company,"ceo":f[0].info_set.all()[0].ceo,"about":f[0].info_set.all()[0].about}
-            return render(request,'Lab/XSS/xss_lab.html',args)
+            return render(request,'Lab/XSS/xss_r_lab.html',args)
         else:
-            return render(request,'Lab/XSS/xss_lab.html', {'query': q})
+            return render(request,'Lab/XSS/xss_r_lab.html', {'query': q})
     else:
         return redirect('login')
 
