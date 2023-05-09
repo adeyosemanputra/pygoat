4a4ea45a18f780d9e7c4ed47f3449595234ebed1
PavelLinearB <pavel.vaks@linearb.io>
2023-05-09 15:49:34 +0300

    fix bugs

--- introduction/apis.py
+++ introduction/apis.py
@@ -4,7 +4,6 @@ from introduction.playground.ssrf import main
 from introduction.playground.A9.main import Log
 from introduction.playground.A6.utility import check_vuln
 from django.contrib.auth import login,authenticate
-from .utility import *
 from django.views.decorators.csrf import csrf_exempt
 import time
 from .views import authentication_decorator
@@ -20,31 +19,10 @@ def ssrf_code_checker(request):
         if request.method == 'POST':
             python_code = request.POST['python_code']
             html_code = request.POST['html_code']
-            if not (ssrf_code_converter(python_code)):
-                return JsonResponse({"status": "error", "message": "Invalid code"})
-            test_bench1 = ssrf_html_input_extractor(html_code)
-            
-            if (len(test_bench1) >4):
-                return JsonResponse({'message':'too many inputs in Html\n Try again'},status = 400)
-            test_bench2 = ['secret.txt']
-            correct_output1 = [{"blog": "blog1-passed"}, {"blog": "blog2-passed"}, {"blog": "blog3-passed"}, {"blog": "blog4-passed"}]
-            outputs = []
-            for inputs in test_bench1:
-                outputs.append(main.ssrf_lab(inputs))
-            if outputs == correct_output1:
-                outputs = []
-            else:
-                return JsonResponse({'message':'Testbench failed, Code is not working\n Try again'},status = 200)
-
-            correct_output2 = [{"blog": "No blog found"}]
-            for inputs in test_bench2:
-                outputs.append(main.ssrf_lab(inputs))
-            if outputs == correct_output2:
-                return JsonResponse({'message':'Congratulation, you have written a secure code.', 'passed':1}, status = 200)
             
             return JsonResponse({'message':'Test bench passed but the code is not secure'}, status = 200,safe = False)
         else:
-            return JsonResponse({'message':'method not allowed'},status = 405)
+            return JsonResponse({'message':'1method not allowed'},status = 405)
     else:
         return JsonResponse({'message':'UnAuthenticated User'},status = 401)
 
@@ -82,7 +60,7 @@ def log_function_checker(request):
         f.close()
         return JsonResponse({"message":"success", "logs": lines},status = 200)
     else:
-        return JsonResponse({"message":"method not allowed"},status = 405)
+        return JsonResponse({"message":"2method not allowed"},status = 405)
 
 #a7 codechecking api
 @csrf_exempt
@@ -120,7 +98,7 @@ def A6_disscussion_api(request):
 @csrf_exempt
 def A6_disscussion_api_2(request):
     if request.method != 'POST':
-        return JsonResponse({"message":"method not allowed"},status = 405)
+        return JsonResponse({"message":"3method not allowed"},status = 405)
     try:
         code = request.POST.get('code')
         dirname = os.path.dirname(__file__)
