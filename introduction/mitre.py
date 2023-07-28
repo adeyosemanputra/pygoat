192ac33af272786d2fc808eebfc626794dc70f79
afoliveir <122925937+afoliveir@users.noreply.github.com>
2023-07-29 00:32:44 +0100

    revert 237 mitre.py

--- introduction/mitre.py
+++ introduction/mitre.py
@@ -233,26 +233,8 @@ def command_out(command):
 
 @csrf_exempt
 def mitre_lab_17_api(request):
-    if request.method == "GET": #request.method == "POST":
-        # addicionar validação
-                    def validate_ip_regex(ip_address):
-            match = re.match(
-                r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", "127.0.0.1", ip_address)
-            
-            if not bool(match):
-                print(f"The IP address {ip_address} is not valid")
-                return False
-
-            bytes = ip_address.split(".")
-            
-            for ip_byte in bytes:
-                if int(ip_byte) < 0 or int(ip_byte) > 255:
-                    print(f"The IP address {ip_address} is not valid")
-                    return False
-            print(f"The IP address {ip_address} is valid")
-            return True
-        #fim de inserção
-        ip = request.POST.get('ip_address') # ip = request.POST.get('ip')
+    if request.method == "POST":
+        ip = request.POST.get('ip_address')
         command = "nmap " + ip 
         res, err = command_out(command)
         res = res.decode()
