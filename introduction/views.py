e88f7b3f2507bdcdc32620e87bb235fc5ad3539f
ngui1975 <nguimaraes@outlook.com>
2023-08-01 08:16:39 +0100

    (

--- introduction/views.py
+++ introduction/views.py
@@ -853,7 +853,7 @@ def injection_sql_lab(request):
 
         if name:
             #SQL Injection (ID='QbmcYqm7jZz+XEa6w6wFaRhEF8g=') sql_query = "SELECT * FROM introduction_sql_lab_table WHERE id='"+name+"'AND password='"+password+"'"
-            sql_query = "SELECT * FROM introduction_sql_lab_table WHERE (name = %s and password = %s)", [name, password])
+            sql_query = ("SELECT * FROM introduction_sql_lab_table WHERE (name = %s and password = %s)", [name, password])
             sql_instance = sql_lab_table(id="admin", password="65079b006e85a7e798abecb99e47c154")
             sql_instance.save()
             sql_instance = sql_lab_table(id="jack", password="jack")
