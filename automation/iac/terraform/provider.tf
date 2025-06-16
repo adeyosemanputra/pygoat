5f60923e5bb374a1257238d3f635c43489aea1f4
gantahari <urs.ghp@gmail.com>
2025-06-16 11:20:45 -0400

    Update provider.tf

--- automation/iac/terraform/provider.tf
+++ automation/iac/terraform/provider.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
-
-    features {}
+     subscription_id = "606e824b-aaf7-4b4e-9057-b459f6a4436d"
+     client_id       = "461d1c19-8063-4d74-88a0-387152f3a80f"
+     client_secret   = "Xfp8Q~S1NP02OXDpARfO7CEKNGErM_JkPaqaedj."
+     tenant_id       = "104e77d4-81e7-4c16-ab44-935220bed6dd"
+     features {}
 }
 
