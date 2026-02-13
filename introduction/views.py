d9e11fa6ae10023e69a6fd6a452cf82c975477ff
ANIRUDH-SJ <anirudhsj2004@gmail.com>
2026-02-14 00:45:39 +0530

    fix: rename shadowed file handle variable in ssrf_lab

--- introduction/views.py
+++ introduction/views.py
@@ -918,8 +918,8 @@ def ssrf_lab(request):
             try :
                 dirname = os.path.dirname(__file__)
                 filename = os.path.join(dirname, file)
-                file = open(filename,"r")
-                data = file.read()
+                f = open(filename,"r")
+                data = f.read()
                 return render(request,"Lab/ssrf/ssrf_lab.html",{"blog":data})
             except:
                 return render(request, "Lab/ssrf/ssrf_lab.html", {"blog": "No blog found"})
