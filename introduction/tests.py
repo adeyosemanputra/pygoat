a9a255390aa171c17074718fcaec9f31941e0a93
Mohit <mohityadav8@users.noreply.github.com>
2026-04-01 18:01:31 +0530

    Add baseline test suite for introduction app to prevent zero test coverage

--- introduction/tests.py
+++ introduction/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class IntroductionViewsTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_random_page(self):
+        response = self.client.get("/introduction/")
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_lab_page(self):
+        response = self.client.get("/Lab/")
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_mitre_page(self):
+        response = self.client.get("/mitre/")
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_registration_page(self):
+        response = self.client.get("/registration/")
+        self.assertNotEqual(response.status_code, 500)
