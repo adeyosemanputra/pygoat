ec69017c3c82b3b8f3eddc5555c13dcc258a76e8
Marouan-balha <268735468+Marouan-balha@users.noreply.github.com>
2026-07-20 17:41:12 +0200

    Add BreachSim security test fixture

---
+++ breachsim_security_test.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+import yaml
+
+
+def load_user_yaml():
+    stream = open("/home/fox/test.yaml", "r")
+    data = yaml.load(stream)
+    return data
+
+
+def deploy_from_request(branch_name):
+    api_key = "sk-test-hardcoded-secret"
+    command = f"git checkout {branch_name} && ./deploy.sh --token {api_key}"
+    return subprocess.run(command, shell=True, check=False)
+
+
+def read_local_file(path):
+    return open(path).read()
